6064950ec5daf53582dac5ec358c044f4dc2de15
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-06-10 11:29:17 -0300

    -- ajustando async da funcao

--- robots/twitch.js
+++ robots/twitch.js
@@ -4,34 +4,30 @@ const state = require('./state.js')
 
 async function robot(){
     const content = state.load()
+    
+    await getMonthClips(content)
 
-    await getMonthClips()
-
-    async function getMonthClips(){
+    state.save(content)
+    
+    async function getMonthClips(content){
         // TODO: Verify Channel is required
         const data = {
             channel: content.channel,
             period: content.period
         }
 
-         axios({
-            method: 'get',
-            url: `https://api.twitch.tv/kraken/clips/top?${qs.stringify(data)}`,
+        let response = await axios.get(`https://api.twitch.tv/kraken/clips/top?${qs.stringify(data)}`, {
             headers: {
                 'Accept': 'application/vnd.twitchtv.v5+json',
                 'Client-ID': 'dlpr96izow24qfp1qf5mlbu7ees7we',
-               
             },
-          }).then(function (response) {
-            content.clips = createVideoProperty(response.data.clips)
-            state.save(content)
-          });
+        })
+
+        content.clips = await createVideoProperty(response.data.clips)
     }
 
     function createVideoProperty (clips) {
-
         return clips.map(clip => ({ ...clip, videoMp4url: clip.thumbnails.medium.replace(/-preview-480x272.jpg/, '.mp4') }))
-
     }
 }
 
