942d8592df6d64c48c4901f8bcd700dffb4ab3bc
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-06-12 10:07:40 -0300

    -- adicionando status

--- robots/downloader.js
+++ robots/downloader.js
@@ -7,19 +7,32 @@ async function robot() {
     const content = await state.load()
     const dir = './temp';
     
+    console.log('> [downloader-robot] Checking temp folder')
     if (!fs.existsSync(dir)){
         fs.mkdirSync(dir);
+    }else {
+        console.log('> [downloader-robot] Cleaning temp folder')
+
+        fs.readdir(dir, (err, files) => {
+            if (err) throw err;
+            for (const file of files) {
+              fs.unlink(path.join(dir, file), err => {
+                if (err) throw err;
+              });
+            }
+        });
+        console.log('> [downloader-robot] Success temp folder is clean')
     }
 
     await downloadVideos(content)
 
     async function downloadVideos(content) {
-        
+        console.log('> [downloader-robot] Starting download clips...')
         const promises = content.clips.map(async (clip) => {
             let video = await axios.get(clip.videoMp4url, {
                 responseType: 'stream'
             })
-            
+            console.log(`> [downloader-robot] Criando ${clip.slug}.mp4`)
             video.data.pipe(fs.createWriteStream(`./temp/${clip.slug}.mp4`))
         });
     
