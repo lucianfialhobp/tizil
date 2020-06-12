faef02bcb1fb788f2c73a936e88de20f949d7f4d
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-06-12 10:06:47 -0300

    -- adicionando index

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const robots = {
     twitch: require('./robots/twitch.js'),
     state: require('./robots/state.js'),
     downloader: require('./robots/downloader.js'),
+    editor: require('./robots/editor.js'),
 }
 
 async function start(){
     robots.input()
     await robots.twitch()
     await robots.downloader()
+    await robots.editor()
 }
 
 start()
